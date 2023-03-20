3464746fcde458034785a857f383eeaa18a85313
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-20 15:32:55 +0530

    Elcome Msg Printing

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystemDB
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Address Book System DB");
+        }
+    }
+}
